a296c5f38a7969dfc225002f3b82b6f01c9ecff5
Diego Barrueta <diego.barrueta@gmail.com>
2024-06-05 11:16:26 -0300

    changes in ProductRepository

--- app/Models/CategoryProduct.php
+++ app/Models/CategoryProduct.php
@@ -10,4 +10,11 @@ class CategoryProduct extends Model
     use HasFactory;
 
     protected $table = 'category_product';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['category_id', 'product_id'];
 }
