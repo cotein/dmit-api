634b87eb9932275f7ac6208c09d9ba7b7baf731f
Diego Barrueta <diego.barrueta@gmail.com>
2025-04-18 09:38:51 -0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -21,9 +21,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('CORS_ALLOW_ORIGIN'), 'http://localhost:5173', 'http://localhost:8888', 'https://www.facturador.dmit.ar', 'https://www.dmit.ar', 'https://emailsender.dmit.ar'],
+    'allowed_origins' => [env('CORS_ALLOW_ORIGIN'), 'http://localhost:5173', 'http://localhost:8888', 'https://www.facturador.dmit.ar', 'https://www.dmit.ar', 'https://emailsender.dmit.ar', 'https://facturador.dmit.ar'],
 
-    'allowed_origins_patterns' => ['/^https?:\/\/(.+\.)?dmit\.ar$/', '/^http:\/\/localhost:5173$/', '/^https:\/\/www\.dmit\.ar$/', '/^http:\/\/localhost:8888$/', '/^https:\/\/www\.facturador\.dmit\.ar$/', '/^https:\/\/.emailsender\.dmit\.ar$/'],
+    'allowed_origins_patterns' => ['/^https?:\/\/(.+\.)?dmit\.ar$/', '/^http:\/\/localhost:5173$/', '/^https:\/\/www\.dmit\.ar$/', '/^http:\/\/localhost:8888$/', '/^https:\/\/www\.facturador\.dmit\.ar$/', '/^https:\/\/.emailsender\.dmit\.ar$/', '/^https:\/\/.facturador\.dmit\.ar$/'],
 
     'allowed_headers' => ['*'],
 
